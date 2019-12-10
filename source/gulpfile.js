759c7ec42ec3e99af718190f1be55f18095b4235
Ali Shahid <ali.shahid.1994@gmail.com>
2019-12-10 14:49:56 +0100

    Added imagemin and clean tasks

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -3,8 +3,13 @@
 const gulp = require('gulp');
 const browsersync = require("browser-sync");
 const livereload = require('gulp-livereload');
+const clean = require('gulp-clean');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
 const sass = require('gulp-sass');
 const cssmin = require('gulp-cssmin');
+const jshint = require('gulp-jshint');
+const notify = require('gulp-notify');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
@@ -22,6 +27,12 @@ function browserSync(done) {
 function reload(done) {
 	browsersync.reload();
 	done();
+}
+
+function cleaning(){
+	return gulp
+		.src(['dist/css', 'dist/js', 'dist/img'], {read: false, allowEmpty: true})
+    .pipe(clean());
 }
 
 function runSass() {
@@ -54,6 +65,8 @@ function vendor() {
 function scripts(){
 	return gulp
 		.src('app/js/app.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
 		.pipe(uglify())
 		.pipe(concat('scripts.js'))
 		.pipe(rename({suffix: '.min'}))
@@ -61,30 +74,34 @@ function scripts(){
 		.pipe(livereload({ start: true }))
 }
 
+function images(){
+	return gulp
+		.src('app/img/*.*')
+		.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('dist/img'))
+		.pipe(notify({ message: 'Images task complete' }))
+		.pipe(livereload({ start: true }))
+}
+
 function move(done){
-    console.log('Moving all HTML and image files to dist folder');
-    gulp.src('app/img/*.*')
-      .pipe(gulp.dest('dist/img'));
+    console.log('Moving HTML files to dist folder');
     gulp.src('app/*.html')
       .pipe(gulp.dest('dist/'));
-    gulp.src('app/files/*.*')
-      .pipe(gulp.dest('dist/files'));
     done();
 }
 
 function watchFiles(){
 	livereload.listen({ basePath: 'dist' })
-
 	gulp.watch('app/scss/**/*.scss', gulp.series(runSass, runCssmin, reload))
 	gulp.watch('app/js/*.js', gulp.series(vendor, scripts, reload))
 	gulp.watch('app/*.html', gulp.series(move, reload))
-	gulp.watch('app/img/*.*', gulp.series(move, reload))
+	gulp.watch('app/img/*.*', gulp.series(images, reload))
 }
 
 const css = gulp.series(runSass, runCssmin);
 const js = gulp.parallel(vendor, scripts);
-const watch = gulp.parallel(watchFiles);
-const defaultTasks = gulp.series(runSass, runCssmin, vendor, scripts, move, browserSync, watch);
+const watch = gulp.series(cleaning, watchFiles);
+const defaultTasks = gulp.series(cleaning, runSass, runCssmin, vendor, scripts, images, move, browserSync, watchFiles);
 
 exports.css = css;
 exports.js = js;
